d811a3fdaabcea21a0c88bf453b7e3b8c91b4aed
MrChen <chenhuiyiyoung@163.com>
2019-01-06 22:57:58 +0800

    Initial Commit

---
+++ HYVedioPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HYVedioPlayer
+//
+//  Created by MrChen on 2019/1/6.
+//  Copyright © 2019 MrChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HYVedioPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HYVedioPlayer
+//
+//  Created by MrChen on 2019/1/6.
+//  Copyright © 2019 MrChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
